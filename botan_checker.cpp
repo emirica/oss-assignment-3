02780e54b13b7e74b7c3348474b6f5f867915f90
Emma Mirica <emma.mirica@gmail.com>
2013-05-12 23:49:28 +0300

    [twofish][botan] Tests big files

--- botan_checker.cpp
+++ botan_checker.cpp
@@ -36,7 +36,7 @@ static std::fstream input_enc;
 static std::ofstream input_dec;
 static std::string algo = "Twofish";
 static std::string mode = "/CBC";
-static std::string file_sizes[] = { "1MB", "8MB", "64MB", "512MB", "1GB" };
+static std::string file_sizes[] = { "2.6KB", "1MB", "8MB", "64MB", "512MB", "1GB" };
 
 static Botan::SecureVector<Botan::byte> b64_decode(const std::string& in)
 {
@@ -94,8 +94,7 @@ static int do_encrypt(std::string passphrase, Botan::SecureVector<Botan::byte> s
         Pipe encryptor(new Fork(
                                 new Chain(
                                           new MAC_Filter("HMAC(SHA-1)", mac_key),
-                                          new Base64_Encoder,
-                                          new DataSink_Stream(input_enc)
+                                          new Base64_Encoder
                                          ),
                                 new Chain(
                                           get_cipher(algo + mode, key, iv, ENCRYPTION),
@@ -108,8 +107,7 @@ static int do_encrypt(std::string passphrase, Botan::SecureVector<Botan::byte> s
         input >> encryptor;
         encryptor.end_msg();
 
-        //input_enc << encryptor.read_all_as_string(0) << std::endl;
-        input_enc << std::endl;
+        input_enc << encryptor.read_all_as_string(0) << std::endl;
         input_enc << encryptor.read_all_as_string(1);
 
     } catch (std::exception& e) {
@@ -211,27 +209,8 @@ int main(int nargc, char *argv[])
     get_str_time(timebuf);
     botan_out << "[" << timebuf << "] === Starting testing botan Twofish implementation ===\n\n";
 
-    // First do a standard test to verify the correctness of the algorithm. The
-    // output obtained (encryption) will be checked against the output
-    // obtained with the implementation from the other two libraries.
+    // First do some standard tests to verify the correctness of the algorithm
     std::string input_name(TEST_FILENAME);
-    input.open((input_name + "1").c_str());
-    if (!input) {
-        std::cerr << "ERROR: couldn't open " << TEST_FILENAME << "1" << std::endl;
-        return 1;
-    }
-    input_enc.open((input_name + "1.enc").c_str(), ios::out);
-    if (!input_enc) {
-        std::cerr << "ERROR: couldn't open " << TEST_FILENAME << "1.enc" << std::endl;
-        return 1;
-    }
-    input_dec.open((input_name + "1.dec").c_str());
-    if (!input_dec) {
-        std::cerr << "ERROR: couldn't open " << TEST_FILENAME << "1.dec" << std::endl;
-        return 1;
-    }
-    get_str_time(timebuf);
-    botan_out << "\t[" << timebuf <<"] === Start: Correctness - Botan Twofish ===\n";
 
     const BlockCipher* cipher_proto = global_state().algorithm_factory().prototype_block_cipher(algo);
     if (!cipher_proto) {
@@ -243,31 +222,9 @@ int main(int nargc, char *argv[])
     const u32bit iv_len = cipher_proto->block_size();
 
     Botan::SecureVector<Botan::byte> salt(8);
-    rng.randomize(&salt[0], salt.size());
+    int rc;
 
-    do_encrypt(passphrase, salt, key_len, iv_len);
-    input_enc.close();
-    input_enc.open((input_name + "1.enc").c_str(), ios::in);
-    if (!input_enc) {
-        std::cerr << "ERROR: couldn't open " << input_name << "1.enc for read\n";
-        return 1;
-    }
-    int rc = do_decrypt(passphrase, key_len, iv_len);
-
-    botan_out << "\t\t Correctness test ";
-    (rc == 0) ? botan_out << "passed\n" : botan_out << "failed\n";
-
-    get_str_time(timebuf);
-    botan_out << "\t[" << timebuf << "] === Stop: Correctness - Botan Twofish ===\n\n";
-    input.close();
-    input_dec.close();
-    input_enc.close();
-
-
-    // Test the speed of this implementation using different file sizes.
-    // The output will be appended to the BOTAN_FILE and in the end the
-    // three implementations will be checked against each other.
-    for (int i = 2; i <= 6; i++) {
+    for (int i = 1; i <= 5; i++) {
         // Open the input file
         std::stringstream ss;
         ss << i;
@@ -288,7 +245,7 @@ int main(int nargc, char *argv[])
         }
         uint64_t start = get_str_time(timebuf);
         botan_out << "\t[" << timebuf <<"] === Start: Speed ";
-        botan_out << file_sizes[i - 2] << " - Botan Twofish ===\n";
+        botan_out << file_sizes[i - 1] << " - Botan Twofish ===\n";
 
         rng.randomize(&salt[0], salt.size());
         do_encrypt(passphrase, salt, key_len, iv_len);
@@ -297,7 +254,7 @@ int main(int nargc, char *argv[])
         // Save encryption duration
         botan_out << "\t\t[" << timebuf << "] === Encryption for ";
         (stop_enc > start) ? diff = stop_enc - start : 0;
-        botan_out << file_sizes[i - 2] << " lasted " << diff << " [us]\n";
+        botan_out << file_sizes[i - 1] << " lasted " << diff << " [us]\n";
 
         input_enc.close();
         input_enc.open((input_name + ss.str() + ".enc").c_str(), ios::in);
@@ -314,15 +271,106 @@ int main(int nargc, char *argv[])
         (rc == 0) ? botan_out << " passed\n" : botan_out << " failed\n";
         (stop > stop_enc) ? diff = stop - stop_enc : diff = 0;
         botan_out << "\t\t[" << timebuf << "] === Decryption for ";
-        botan_out << file_sizes[i - 2] << " lasted " <<  diff << " [us]\n";
+        botan_out << file_sizes[i - 1] << " lasted " <<  diff << " [us]\n";
         (stop > start) ? diff = stop - start : diff = 0;
         botan_out << "\t[" << timebuf <<"] === Stop: Total time ";
-        botan_out << diff << "[us] Speed " << file_sizes[i - 2] << " - Botan Twofish ===\n\n";
+        botan_out << diff << "[us] Speed " << file_sizes[i - 1] << " - Botan Twofish ===\n\n";
+
+        std::cout << "[ " << diff << " us] Correctness: Test for file " << file_sizes[i - 1] << " ended\n";
 
         input.close();
         input_enc.close();
         input_dec.close();
-        std::cout << "[ " << diff << " us] Test for file " << file_sizes[i - 2] << " ended\n";
+
+    }
+
+    // Test the speed of this implementation using different file sizes.
+    // The output will be appended to the BOTAN_FILE and in the end the
+    // three implementations will be checked against each other.
+    // DataSink_Stream allows us to test with big files
+    for (int i = 1; i <= 6; i++) {
+        std::stringstream ss;
+        // Open the input file
+        ss << i;
+        input.open((input_name + ss.str()).c_str());
+        if (!input) {
+            std::cerr << "ERROR: couldn't open " << TEST_FILENAME << i << std::endl;
+            return 1;
+        }
+        input_enc.open((input_name + ss.str() + "_nohash.enc").c_str(), ios::out);
+        if (!input_enc) {
+            std::cerr << "ERROR: couldn't open " << TEST_FILENAME << i << "_nohash.enc" << std::endl;
+            return 1;
+        }
+        input_dec.open((input_name + ss.str() + "_nohash.dec").c_str());
+        if (!input_dec) {
+            std::cerr << "ERROR: couldn't open " << TEST_FILENAME << i << "_nohash.dec" << std::endl;
+            return 1;
+        }
+        uint64_t start = get_str_time(timebuf);
+        botan_out << "\t[" << timebuf <<"] === Start: Speed ";
+        botan_out << file_sizes[i - 1] << " - Botan Twofish ===\n";
+
+        rng.randomize(&salt[0], salt.size());
+        std::auto_ptr<PBKDF> pbkdf(get_pbkdf("PBKDF2(SHA-1)"));
+        const u32bit PBKDF2_ITERATIONS = 8192;
+
+        SymmetricKey key = pbkdf->derive_key(key_len, "BLK" + passphrase,
+                                             &salt[0], salt.size(),
+                                             PBKDF2_ITERATIONS);
+        InitializationVector iv = pbkdf->derive_key(iv_len, "IVL" + passphrase,
+                                                    &salt[0], salt.size(),
+                                                    PBKDF2_ITERATIONS);
+        // Required header for the decryption phase
+        input_enc << ENC_HEADER << std::endl;
+        Pipe encryptor(get_cipher(algo + mode, key, iv, ENCRYPTION),
+                       new DataSink_Stream(input_enc)
+                      );
+        encryptor.start_msg();
+        input >> encryptor;
+        encryptor.end_msg();
+
+        uint64_t stop_enc = get_str_time(timebuf);
+
+        // Save encryption duration
+        botan_out << "\t\t[" << timebuf << "] === Encryption for ";
+        (stop_enc > start) ? diff = stop_enc - start : 0;
+        botan_out << file_sizes[i - 1] << " lasted " << diff << " [us]\n";
+
+        input_enc.close();
+        input_enc.open((input_name + ss.str() + "_nohash.enc").c_str(), ios::in);
+        if (!input_enc) {
+            std::cerr << "ERROR: couldn't open " << TEST_FILENAME << i << ".enc for read" << std::endl;
+            return 1;
+        }
+
+        std::string header;
+        std::getline(input_enc, header);
+        if (header != ENC_HEADER) {
+            std::cerr << "ERROR: File to decrypt doesn't contain encryption" << std::endl;
+            return 1;
+        }
+        Pipe decryptor(get_cipher(algo + mode, key, iv, DECRYPTION),
+                       new DataSink_Stream(input_dec)
+                      );
+        decryptor.start_msg();
+        input_enc >> decryptor;
+        decryptor.end_msg();
+        uint64_t stop = get_str_time(timebuf);
+
+        // Write useful output
+        (stop > stop_enc) ? diff = stop - stop_enc : diff = 0;
+        botan_out << "\t\t[" << timebuf << "] === Decryption for ";
+        botan_out << file_sizes[i - 1] << " lasted " <<  diff << " [us]\n";
+        (stop > start) ? diff = stop - start : diff = 0;
+        botan_out << "\t[" << timebuf <<"] === Stop: Total time ";
+        botan_out << diff << "[us] Speed " << file_sizes[i - 1] << " - Botan Twofish ===\n\n";
+
+        input.close();
+        input_enc.close();
+        input_dec.close();
+        std::cout << "[ " << diff << " us] Speed: Test for file " << file_sizes[i - 1] << " ended\n";
+
     }
 
     get_str_time(timebuf);
